980eedcd8a6d965433d32098cbfcf732e3f8cc19
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-28 03:27:00 +0530

    Card List Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { CardList } from './components/card-list/card-list.component';
 
 const App = () => {
+
+  const [names, setNames] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setNames(data);
+      })
+  }, []);
+
   return (
     <div className="app">
-      <h1>App</h1>
+      <CardList>
+        {
+          names && names.map((item) => (
+            <h1 key={item.id}>{item.name}</h1>
+          ))
+        }
+      </CardList>
     </div>
   )
 }
--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.listen(PORT, () => {
-    console.log(`App is listening at ${PORT}`);
+    console.log(`App is running at http://localhost:${PORT}`);
 })
